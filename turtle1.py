71eeb028810c565d494d5bb2eebc12f1ed0891e9
bonaventura-p <bonaventurapacileo@gmail.com>
2015-06-29 11:12:58 +0200

    turtle1

---
+++ turtle1.py
@@ -0,0 +1,29 @@
+#turtle quiz 1
+from turtle import *
+
+pencolor('pink')
+circle(100)
+pencolor ('red')
+circle(97)
+pencolor('green')
+circle(95)
+
+begin_fill()
+pencolor('pink')
+circle(100)
+pencolor ('red')
+circle(97)
+pencolor('green')
+circle(95)
+end_fill()
+
+#turtle quiz 2
+for _ in range(6):
+	pd()
+	fd(70)
+	left(60)
+
+#
+
+
+
